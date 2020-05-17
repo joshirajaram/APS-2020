bf285185214c6ee4c4a1d436ba48b438fbc11129
Rajaram M Joshi <37036365+joshirajaram@users.noreply.github.com>
2020-05-18 00:39:02 +0530

    Create 107-CIRCLES2-CodeChef.py

---
+++ CodeLib/107-CIRCLES2-CodeChef.py
@@ -0,0 +1,12 @@
+def circle(x1, y1, x2,y2, r1, r2): 
+    distSq = (((x1 - x2)* (x1 - x2))+ ((y1 - y2)* (y1 - y2)))**(.5) 
+    if (distSq + r1 <= r2): 
+        print("NO") 
+    else: 
+        print("YES") 
+
+T = int(input())
+for _ in range(T):
+    (x1,y1,x2,y2,r1,r2) = list(map(int,input().split(' ')))
+    
+    circle(x1, y1, x2, y2, r1, r2)
